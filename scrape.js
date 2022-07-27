e4cadf63919b6ede7cb6c360cefec43f4ac2ebd1
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2022-07-26 22:00:16 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,23 +3,29 @@ const puppeteer = require("puppeteer")
 const fs = require("fs/promises")
 
 async function start() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto("https://en.wikipedia.org/wiki/Chiang_Kai-shek", {waitUntil : 'domcontentloaded'})
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://en.wikipedia.org/wiki/Chiang_Kai-shek", {waitUntil : 'domcontentloaded'})
 
-  const photos = await page.$$eval("img", imgs => {
-    return imgs.map(x => x.src)
-  })
-
-  for (const photo of photos) {
-    const imagepage = await page.goto(photo)
-    const filename = "images"
-    await fs.writeFile(filename + "/" + photo.split("/").pop(), await imagepage.buffer(), function(err){
-        if (err) throw err;
+    const photoNames = await page.evaluate(() => {
+        const imageNames = Array.from(document.querySelectorAll(".thumbcaption"))
+        let nameArray = [...imageNames]
+        fs.writeFile(names.txt, nameArray.map(h=>h.innerText));
     })
-  }
 
-  await browser.close()
+    const photos = await page.$$eval("img", imgs => {
+        return imgs.map(x => x.src)
+    })
+
+    for (const photo of photos) {
+        const imagepage = await page.goto(photo)
+        const filename = "images"
+        await fs.writeFile(filename + "/" + photo.split("/").pop(), await imagepage.buffer(), function(err){
+            if (err) throw err;
+        })
+    }
+
+    await browser.close()
 }
 
 start()
